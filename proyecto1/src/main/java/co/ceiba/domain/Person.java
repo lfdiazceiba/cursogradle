d3b77ad5e76fc5a3c908e2782ef81a229bde39cb
Leonardo Fabio Díaz Álvarez  - Ceiba Software <leonardo.diaz@ceiba.com.co>
2017-12-28 14:00:31 -0500

    commit initial

---
+++ proyecto1/src/main/java/co/ceiba/domain/Person.java
@@ -0,0 +1,22 @@
+package co.ceiba.domain;
+
+public class Person {
+	
+	private String name;
+	
+	private String lastName;
+	
+	public Person(String name, String lastName) {
+		this.name = name;
+		this.lastName = lastName;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+
+}
---
+++ proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -0,0 +1,30 @@
+package co.ceiba.service;
+
+import co.ceiba.domain.Person;
+import co.ceiba.testdatabuilder.PersonTestDataBuilder;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NotifyPersonServiceTest {
+
+	private NotifyPersonService notifyPersonService;
+	private EmailService emailService;
+	
+	@Before
+	public void setUp() {
+		emailService = new EmailService();
+		notifyPersonService = new NotifyPersonService(emailService);
+	}
+	
+	@Test
+	public void notifyTest() {
+		// Arrange
+		Person person = new PersonTestDataBuilder().build();
+		//Act
+		String message = notifyPersonService.notify(person);
+		//Assert
+		assertNotNull(message);
+	}
+}
